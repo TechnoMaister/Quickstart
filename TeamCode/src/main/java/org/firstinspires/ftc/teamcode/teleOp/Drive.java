9473cb7a43467be103bada4d555d62dfa875d6ae
TechnoMaister <91263932+TechnoMaister@users.noreply.github.com>
2025-10-31 14:54:15 +0200

    second

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/Drive.java
@@ -1,73 +1,29 @@
 package org.firstinspires.ftc.teamcode.teleOp;
 
-import static util.RobotConstants.CLAW_CLOSED;
-import static util.RobotConstants.CLAW_DOWN;
-import static util.RobotConstants.CLAW_MID;
-import static util.RobotConstants.CLAW_OPEN;
-import static util.RobotConstants.CLAW_ROT_45;
-import static util.RobotConstants.CLAW_ROT_OR;
-import static util.RobotConstants.CLAW_ROT_VR;
-import static util.RobotConstants.CLAW_UP_BASKET;
-import static util.RobotConstants.CLAW_UP_CHAMBER;
-import static util.RobotConstants.CLAW_UP_SUB;
-import static util.RobotConstants.DOWN;
-import static util.RobotConstants.EXT;
-import static util.RobotConstants.HANG;
-import static util.RobotConstants.HIGH_CHAMBER;
-import static util.RobotConstants.INTAKE;
-import static util.RobotConstants.LOW_BASKET;
-import static util.RobotConstants.MEXT;
-import static util.RobotConstants.UNEXT;
-
-import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import util.Encoder;
-import util.Hardware;
+import org.firstinspires.ftc.teamcode.util.Hardware;
 
-@Config
-@TeleOp(group = "teleOp")
+@TeleOp
 public class Drive extends OpMode {
-
     public Hardware robot;
-    public Encoder encoder;
-    public Timer intakeTime, extSubT, submersibleTime, basket, timer;
     public Gamepad previousGamepad1, currentGamepad1;
-    public boolean L1, CROSS, CIRCLE, TRIANGLE;
-    public double clawWristPos, clawPos, clawRotPos;
-    public int liftTargetPos, extTargetPos, clawRot;
-    public static double
-
-    intakeTimeV = 500,
-
-    intakeExt = 1000,
-
-    extSubTV = 500,
-
-    submersibleTimeV = 500,
-
-    submersibleTimeV2 = 1000,
-
-    basketV = 900,
-
-    timerV = 500;
+    public Timer rumbling;
+    public int direction;
 
     @Override
     public void init() {
         robot = new Hardware(hardwareMap);
-        encoder = new Encoder();
+
+        rumbling = new Timer();
 
         previousGamepad1 = new Gamepad();
         currentGamepad1 = new Gamepad();
-
-        intakeTime = new Timer();
-        extSubT = new Timer();
-        submersibleTime = new Timer();
-        basket = new Timer();
-        timer = new Timer();
     }
 
     @Override
@@ -77,72 +33,39 @@ public class Drive extends OpMode {
         previousGamepad1.copy(currentGamepad1);
         currentGamepad1.copy(gamepad1);
 
-        switch (clawRot) {
+        if(currentGamepad1.dpad_down && !previousGamepad1.dpad_down) direction++;
+
+        switch (direction) {
             case 0: {
-                clawRotPos = CLAW_ROT_VR;
+                robot.colector.setDirection(DcMotorSimple.Direction.FORWARD);
+                gamepad1.setLedColor(0, 1, 0, Gamepad.LED_DURATION_CONTINUOUS);
+                rumbling.resetTimer();
                 break;
             }
             case 1: {
-                clawRotPos = CLAW_ROT_45;
+                robot.colector.setDirection(DcMotorSimple.Direction.REVERSE);
+                gamepad1.setLedColor(1, 0, 0, Gamepad.LED_DURATION_CONTINUOUS);
+                if(rumbling.getElapsedTime() <= 250) gamepad1.rumble(Gamepad.RUMBLE_DURATION_CONTINUOUS);
+                else gamepad1.stopRumble();
                 break;
             }
-            case 2: {
-                clawRotPos = CLAW_ROT_OR;
-                break;
-            }
-            case 3: {
-                clawRot = 0;
+            default: {
+                direction = 0;
                 break;
             }
         }
 
-        encoder.runTo(robot.leftLift, liftTargetPos);
-        encoder.runTo(robot.rightLift, liftTargetPos);
+        if(gamepad1.cross) robot.colector.setPower(1);
+        else robot.colector.setPower(0);
 
-        encoder.runTo(robot.extend, extTargetPos);
-        robot.clawWrist.setPosition(clawWristPos);
-        robot.claw.setPosition(clawPos);
-        robot.clawRotation.setPosition(clawRotPos);
-
-        if(currentGamepad1.left_bumper && !previousGamepad1.left_bumper && !CIRCLE) {
-            L1 = !L1;
-            intakeTime.resetTimer();
-            submersibleTime.resetTimer();
-            extSubT.resetTimer();
-            timer.resetTimer();
-        }
-        if(currentGamepad1.cross && !previousGamepad1.cross && !CIRCLE && !L1) CROSS = !CROSS;
-        if(currentGamepad1.circle && !previousGamepad1.circle && CROSS && !L1) {
-            basket.resetTimer();
-            CIRCLE = !CIRCLE;
-        }
-        if(currentGamepad1.triangle && !previousGamepad1.triangle && CROSS && !L1 && !CIRCLE) TRIANGLE = !TRIANGLE;
-
-        if(TRIANGLE) {
-            if(L1) hang();
-            else hangIdle();
-        } else {
-            if(CIRCLE) {
-                scoreBasket();
-            } else {
-                if(basket.getElapsedTime() >= basketV) {
-                    if(CROSS) {
-                        if(L1) intakeSubmersible();
-                        else idleSubmersible();
-                    }
-                    else {
-                        if(L1) scoreSpecimen();
-                        else intakeSpecimen();
-                    }
-                } else clawPos = CLAW_OPEN;
-            }
-        }
+        if(gamepad1.circle) for(DcMotorEx motor : robot.shooters) motor.setPower(1);
+        else for(DcMotorEx motor : robot.shooters) motor.setPower(0);
     }
 
     public void drive(Gamepad gamepad) {
-        double y = -gamepad.left_stick_y;
+        double y = gamepad.left_stick_y;
         double x = gamepad.left_stick_x;
-        double rx = gamepad.right_stick_x;
+        double rx = -gamepad.right_stick_x;
 
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double leftFrontPower = (y + x + rx) / denominator;
@@ -154,71 +77,5 @@ public class Drive extends OpMode {
         robot.leftRear.setPower(leftRearPower);
         robot.rightFront.setPower(rightFrontPower);
         robot.rightRear.setPower(rightRearPower);
-    }
-
-    public void intakeSpecimen() {
-        clawRot = 0;
-        extTargetPos = UNEXT;
-        clawPos = CLAW_OPEN;
-        if(timer.getElapsedTime() >= timerV) {
-            liftTargetPos = INTAKE;
-            clawWristPos = CLAW_MID;
-        }
-    }
-
-    public void scoreSpecimen() {
-        clawRot = 0;
-        clawPos = CLAW_CLOSED;
-        if(intakeTime.getElapsedTime() >= intakeTimeV) {
-            liftTargetPos = HIGH_CHAMBER;
-            clawWristPos = CLAW_UP_CHAMBER;
-            if(intakeTime.getElapsedTime() >= intakeExt) extTargetPos = MEXT;
-        } else extTargetPos = UNEXT;
-    }
-
-    public void idleSubmersible() {
-        liftTargetPos = DOWN;
-        if(submersibleTime.getElapsedTime() >= submersibleTimeV)
-            if(submersibleTime.getElapsedTime() >= submersibleTimeV2) {
-                extTargetPos = UNEXT;
-                clawRot = 0;
-                if(currentGamepad1.right_bumper && !previousGamepad1.right_bumper) clawPos = CLAW_OPEN;
-            }
-            else clawWristPos = CLAW_UP_SUB;
-        else clawPos = CLAW_CLOSED;
-    }
-
-    public void intakeSubmersible() {
-        liftTargetPos = DOWN;
-        if(extSubT.getElapsedTime() >= extSubTV) clawWristPos = CLAW_DOWN;
-        else {
-            extTargetPos = EXT;
-            clawPos = CLAW_OPEN;
-        }
-        if(currentGamepad1.right_bumper && !previousGamepad1.right_bumper) clawRot++;
-    }
-
-    public void scoreBasket() {
-        liftTargetPos = LOW_BASKET;
-        extTargetPos = MEXT;
-        clawWristPos = CLAW_UP_BASKET;
-        clawRot = 2;
-        clawPos = CLAW_CLOSED;
-    }
-
-    public void hangIdle() {
-        liftTargetPos = 0;
-        extTargetPos = UNEXT;
-        clawWristPos = CLAW_MID;
-        clawRot = 0;
-        clawPos = CLAW_OPEN;
-    }
-
-    public void hang() {
-        liftTargetPos = HANG;
-        extTargetPos = UNEXT;
-        clawWristPos = CLAW_MID;
-        clawRot = 0;
-        clawPos = CLAW_OPEN;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Hardware.java
@@ -1,77 +1,37 @@
 package org.firstinspires.ftc.teamcode.util;
 
-import static com.pedropathing.follower.FollowerConstants.leftFrontMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.leftFrontMotorName;
-import static com.pedropathing.follower.FollowerConstants.leftRearMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.leftRearMotorName;
-import static com.pedropathing.follower.FollowerConstants.rightFrontMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.rightFrontMotorName;
-import static com.pedropathing.follower.FollowerConstants.rightRearMotorDirection;
-import static com.pedropathing.follower.FollowerConstants.rightRearMotorName;
-
-import com.pedropathing.util.Constants;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 import java.util.Arrays;
 import java.util.List;
 
-import pedroPathing.constants.FConstants;
-import pedroPathing.constants.LConstants;
-
 public class Hardware {
 
-    public DcMotorEx leftFront, leftRear, rightFront, rightRear, leftLift, rightLift, extend;
-    public List<DcMotorEx> motors, lift;
-    public Servo clawWrist, clawRotation, claw;
+    public DcMotorEx leftFront, leftRear, rightFront, rightRear, colector, leftShoot, rightShoot;
+    public List<DcMotorEx> motors, shooters;
 
-    public Hardware(HardwareMap robot) {
-        Constants.setConstants(FConstants.class, LConstants.class);
+    public Hardware(HardwareMap robot){
+        leftFront = robot.get(DcMotorEx.class, "leftFront");
+        leftRear = robot.get(DcMotorEx.class, "leftRear");
+        rightRear = robot.get(DcMotorEx.class, "rightFront");
+        rightFront = robot.get(DcMotorEx.class, "rightRear");
 
-        leftFront = robot.get(DcMotorEx.class, leftFrontMotorName);
-        leftRear = robot.get(DcMotorEx.class, leftRearMotorName);
-        rightRear = robot.get(DcMotorEx.class, rightRearMotorName);
-        rightFront = robot.get(DcMotorEx.class, rightFrontMotorName);
+        colector = robot.get(DcMotorEx.class, "colector");
+        leftShoot = robot.get(DcMotorEx.class, "leftShoot");
+        rightShoot = robot.get(DcMotorEx.class, "rightShoot");
 
-        leftFront.setDirection(leftFrontMotorDirection);
-        leftRear.setDirection(leftRearMotorDirection);
-        rightFront.setDirection(rightFrontMotorDirection);
-        rightRear.setDirection(rightRearMotorDirection);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        motors = Arrays.asList(leftFront, leftRear, rightFront, rightRear);
+        rightShoot.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        for (DcMotorEx motor : motors) {
-            MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
-            motorConfigurationType.setAchieveableMaxRPMFraction(1.0);
-            motor.setMotorType(motorConfigurationType);
-        }
+        motors = Arrays.asList(leftFront, leftRear, rightFront, rightRear, colector, leftShoot, rightShoot);
+        shooters = Arrays.asList(leftShoot, rightShoot);
 
         for (DcMotorEx motor : motors) {
             motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         }
-
-        leftLift = robot.get(DcMotorEx.class, "leftLift");
-        rightLift = robot.get(DcMotorEx.class, "rightLift");
-
-        rightLift.setDirection(DcMotorEx.Direction.REVERSE);
-        leftLift.setDirection(DcMotorEx.Direction.FORWARD);
-
-        lift = Arrays.asList(leftLift, rightLift);
-
-        for (DcMotorEx motor : lift) {
-            motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
-
-        extend = robot.get(DcMotorEx.class, "extend");
-
-        extend.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-
-        clawWrist = robot.get(Servo.class, "clawWrist");
-        clawRotation = robot.get(Servo.class, "clawRotation");
-        claw = robot.get(Servo.class, "claw");
     }
 }
