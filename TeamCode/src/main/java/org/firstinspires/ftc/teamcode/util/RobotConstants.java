9cf669ac112510283bd5b160c4452044023b3d17
bl1nk15 <128423127+bl1nk15@users.noreply.github.com>
2025-11-13 19:56:36 +0200

    pedro update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotConstants.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.util;
 
-import com.acmerobotics.dashboard.config.Config;
+import com.bylazar.configurables.annotations.Configurable;
 
-@Config
+@Configurable
 public class RobotConstants {
     public static double
 
